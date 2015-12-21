5c43e704a498487d7a01405f7ce3bda47a98c74f
vagrant <vagrant@precise32.(none)>
2015-12-21 18:17:45 +0000

    Examples 1-5 and Makefile

---
+++ Makefile
@@ -0,0 +1,12 @@
+CFLAGS=-Wall -g
+
+all:
+	ex1
+	ex3
+	ex4
+	ex5
+clean:
+	rm -f ex1
+	rm -f ex3
+	rm -f ex4
+	rm -f ex5
---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("It's me");
+    puts("Mario");
+    puts("and Luigi");
+    puts("Let's play Mario Kart");
+    return 0;
+}
