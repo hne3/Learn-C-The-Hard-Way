9f17f2605fee20cea55cf5089a0da7821bd61de7
Haley <hne3@case.edu>
2016-01-01 00:36:42 +0000

    Added extra credit char-by-char writing function

--- ex24.c
+++ ex24.c
@@ -26,33 +26,58 @@ typedef struct Person
   float income;
 } Person;
 
+int write_firstname_lastname(int firstname_length, char *first_name,
+			      int lastname_length, char *last_name,
+			      Person *you)
+{
+  // Check for MAX_DATA - 1 to account for nul byte at the end
+  check(firstname_length < MAX_DATA - 1, "First name too long.");
+  check(lastname_length < MAX_DATA - 1, "Last name too long.");
+  
+  int i = 0;
+  
+  for(i = 0; i < firstname_length; i++)
+    {
+      you->first_name[i] = *(first_name + i);
+    }
+  you->first_name[i + 1] = '\0';
+
+  for(i = 0; i < lastname_length; i++)
+    {
+      you->last_name[i] = *(last_name + i);
+    }
+  you->last_name[i + 1] = '\0';
+
+  return 0;
+
+ error:
+  return 1;
+}
+
+
 int main(int argc, char *argv[])
 {
   Person you = {.age = 0};
   
   // Extra credit: take everything as arguments
-  if(argc != 6)
+  if(argc != 8)
     {
-      log_err("USAGE: ex24 firstname lastname age eyecolor income");
+      log_err("USAGE: ex24 firstname_length firstname lastname_length lastname age eyecolor income");
       return 1;
     }
-  check(argv[1] != NULL, "Failed to read first name.");
-  check(sizeof(argv[1]) < sizeof(char) * MAX_DATA, "First name too long.");
-  strcpy(you.first_name, argv[1]);
-
-  check(argv[2] != NULL, "Failed to read last name.");
-  check(sizeof(argv[2]) < sizeof(char) * MAX_DATA, "Last name too long.");
-  strcpy(you.last_name, argv[2]);
+  check(argv[2] != NULL, "Failed to read first name.");
+  check(argv[4] != NULL, "Failed to read last name.");
+  write_firstname_lastname(atoi(argv[1]), argv[2], atoi(argv[3]), argv[4], &you);
   
-  check(atoi(argv[3]) > 0, "You must enter an age.");
-  you.age = atoi(argv[3]);
+  check(atoi(argv[5]) > 0, "You must enter an age.");
+  you.age = atoi(argv[5]);
 
-  check(atoi(argv[4]) > 0, "You must enter an integer value 1 - 5.");
-  check(atoi(argv[4]) <= OTHER_EYES, "You must enter an integer value 1 - 5."); 
-  you.eyes = atoi(argv[4]) - 1;
+  check(atoi(argv[6]) > 0, "You must enter an integer value 1 - 5.");
+  check(atoi(argv[6]) <= OTHER_EYES, "You must enter an integer value 1 - 5."); 
+  you.eyes = atoi(argv[6]) - 1;
 
-  check(atof(argv[5]) > 0, "You must enter a salary.");
-  you.income = atof(argv[5]);
+  check(atof(argv[7]) > 0, "You must enter a salary.");
+  you.income = atof(argv[7]);
   /*char *in = NULL;
 
   printf("What's your first name? ");
